ac8117fc02ac8d0735a106c246620f256c52a0fe
fullstack-overkill <christophermms.ctt@gmail.com>
2022-05-04 22:04:31 -0300

    refactor: consume of the header and footer components

--- src/presentation/pages/login/login.tsx
+++ src/presentation/pages/login/login.tsx
@@ -1,15 +1,13 @@
 import React from 'react'
 import styles from './login-styles.scss'
 import Spinner from '@/presentation/components/spinner/spinner'
-import Logo from '@/presentation/components/logo/logo'
+import Header from '@/presentation/components/login-header/login-header'
+import Footer from '@/presentation/components/footer/footer'
 
 const login: React.FC = () => {
   return (
     <div className={styles.login}>
-      <header className={styles.header}>
-        <Logo />
-        <h1>4Dev - Enquentes para programadores</h1>
-      </header>
+      <Header />
       <form className={styles.form}>
         <h2>Login</h2>
         <div className={styles.inputWrap}>
@@ -29,7 +27,7 @@ const login: React.FC = () => {
           <span className={styles.error}>Erro</span>
         </div>
       </form>
-      <footer className={styles.footer}></footer>
+      <Footer />
     </div>
   )
 }
