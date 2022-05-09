f89ba5a66bca750efe0794d1c8ad5bd919839b07
fullstack-overkill <christophermms.ctt@gmail.com>
2022-05-09 20:40:48 -0300

    refactor: move global from router component

--- src/presentation/components/router/router.tsx
+++ src/presentation/components/router/router.tsx
@@ -1,7 +1,5 @@
 import React from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import '@/presentation/styles/global.scss'
-
 import { Login } from '@/presentation/pages'
 
 const Router: React.FC = () => {
