34834d08a5cb5bbd85669eb80f8a497934315ca9
fullstack-overkill <christophermms.ctt@gmail.com>
2022-05-09 19:06:31 -0300

    feat: router

---
+++ src/presentation/components/router/router.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import '@/presentation/styles/global.scss'
+
+import { Login } from '@/presentation/pages'
+
+const Router: React.FC = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default Router
